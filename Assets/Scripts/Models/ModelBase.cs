6bafd21424793044c800ab1a1b984bb55064c5ef
nheinstein10 <46773036+nheinstein10@users.noreply.github.com>
2020-07-04 15:51:23 +0700

    add ModelBase

---
+++ Assets/Scripts/Models/ModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using System.ComponentModel;
+
+namespace BattleCity {
+	public class ModelBase : INotifyPropertyChanged {
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void RaisedPropertyChanged(string propertyName) {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
