f8ef1b616f03b1d0842299142b484cbe8671edbc
nheinstein10 <46773036+nheinstein10@users.noreply.github.com>
2020-07-06 23:25:07 +0700

    add UI view base

---
+++ Assets/Scripts/Views/UIViewBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace BattleCity {
+	public abstract class UIViewBase : MonoBehaviour {
+		protected virtual void Start() {
+			InitView();
+		}
+
+		public void InitView() {
+			OnViewInit();
+		}
+
+		protected virtual void OnViewInit() { }
+
+		protected virtual void AddClick(Button control, UnityAction onclick) {
+			Debug.AssertFormat(control, "{0} button cannot be null!", name);
+			control.onClick.AddListener(onclick);
+		}
+	}
+}
