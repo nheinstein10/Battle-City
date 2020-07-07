c3a7fece079e8ec7330660b61825e783fd03c378
nheinstein10 <46773036+nheinstein10@users.noreply.github.com>
2020-07-07 18:24:37 +0700

    add State Machine

---
+++ Assets/Scripts/Gameplay/MapBoundWall.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCity {
+	public class MapBoundWall : MonoBehaviour {
+		
+	}
+}
---
+++ Assets/Scripts/StateMachine/State.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCity {
+	public abstract class StateBase {
+
+	}
+
+	public abstract class State<TAgent> : StateBase {
+
+	}
+}
