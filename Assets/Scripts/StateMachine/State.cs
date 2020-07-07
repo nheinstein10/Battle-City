b83a4eac20e370304ec768b2c3174bad090c2528
nheinstein10 <46773036+nheinstein10@users.noreply.github.com>
2020-07-08 00:01:01 +0700

    update State machine

--- Assets/Scripts/StateMachine/State.cs
+++ Assets/Scripts/StateMachine/State.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 namespace BattleCity {
 	public abstract class StateBase {
+		protected StateMachine stateMachine;
 
+		protected StateBase(StateMachine stateMachine) {
+			this.stateMachine = stateMachine;
+		}
+
+		public virtual void Enter() { }
+
+		public virtual void LogicUpdate() { }
+
+		public virtual void PhysicsUpdate() { }
+
+		public virtual void Exit() { }
 	}
 
 	public abstract class State<TAgent> : StateBase {
+		protected TAgent agent;
 
+		protected State(TAgent agent, StateMachine stateMachine) : base(stateMachine) {
+			this.agent = agent;
+		}
 	}
 }
--- Assets/Scripts/StateMachine/StateMachine.cs
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 namespace BattleCity {
 	public class StateMachine {
+		public StateBase CurrentState { get; private set; }
 
+		public void Initialize(StateBase startingState) {
+			CurrentState = startingState;
+			startingState.Enter();
+		}
+
+		public void ChangeState(StateBase newState) {
+			if (newState == CurrentState) {
+				return;
+			}
+
+			CurrentState.Exit();
+
+			CurrentState = newState;
+			newState.Enter();
+		}
 	}
 }
