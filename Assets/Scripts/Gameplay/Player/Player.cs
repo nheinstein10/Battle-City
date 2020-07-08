3f7fc3426859bdd28ccd6a0f1782707676a4d929
nheinstein10 <46773036+nheinstein10@users.noreply.github.com>
2020-07-08 17:07:52 +0700

    State machine structuring

---
+++ Assets/Scripts/Gameplay/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCity {
+    public class Player : MonoBehaviour {
+        public Animator animator;
+
+        public StateMachine SM_Player { get; private set; }
+        public States states { get; private set; }
+
+        public static event System.Action<Player> onPlayerSpawn;
+        public static event System.Action<Player> onPlayerDie;
+
+        private void Update() {
+            
+        }
+
+        private void FixedUpdate() {
+            
+        }
+
+        public class States {
+            public States(Player player, StateMachine smPlayer) {
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Player/PlayerStates/STPlayerSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCity {
+	public class STPlayerSpawn : State<Player> {
+		public STPlayerSpawn(Player agent, StateMachine stateMachine) : base(agent, stateMachine) {
+		}
+
+		public override void Enter() {
+			
+		}
+
+		public override void LogicUpdate() {
+			
+		}
+	}
+}
